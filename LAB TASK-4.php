7242dd291a49687978dc4e1631c562e12e316d5f
imdadhaq <imdadhaque2210@gmail.com>
2021-09-27 23:08:05 +0600

    Lab 2 php placement

---
+++ LAB TASK-4.php
@@ -0,0 +1,15 @@
+<?php
+    $number=20;
+    $number1=30;
+
+    if($number <$number1 )
+    {
+        echo "$number is greater "; 
+        
+    }
+    else{
+        echo "$number1 is greater ";
+         
+    }
+
+?>
---
+++ LAB TASK-5.php
@@ -0,0 +1,10 @@
+<?php
+ 
+$num = 1;
+while ( $num <= 100 ) {
+  print "$num";
+  echo "<br>";
+  $num += 2;
+}
+ 
+?>
